14d839e278a858b6b17a2e943f20b6f056f33498
Nicholas Smith <nicholas.smith@falconsocial.com>
2016-04-14 21:15:32 +0200

    wip

--- capstone/server.R
+++ capstone/server.R
@@ -1,23 +1,37 @@
 library(shiny)
-library(ggplot2)
 
 bigrams <- read.csv('bigrams.csv')
 trigrams <- read.csv('trigrams.csv')
 
-shinyServer(
-  function(input, output) {
+shinyServer(function(input, output) {
+  
+  bigramPrediction <- reactive({
+    term <- paste("^", input$var, sep = "")
+    bigramsFound <- bigrams[grep(term, bigrams$bigram),]
+    bigramsFound <- bigramsFound[order(bigramsFound$Frequency, decreasing = TRUE), ]
+    bigramsFound[1:3, 1]
+  })
+  
+  trigramPrediction <- reactive({
+    if (length(grep(" ", input$var)) == 1) {
+      term <- paste("^", input$var, sep = "")
+      trigramsFound <- trigrams[grep(term, trigrams$trigram),]
+      trigramsFound <- trigramsFound[order(trigramsFound$Frequency, decreasing = TRUE), ]
+      trigramsFound[1:3, 1]
+    }
+  })
+  
+  output$prediction <- renderUI({
+    biPred <- bigramPrediction()
+    all <- c(as.character(biPred))
     
-    bigramPrediction <- reactive({
-        term <- paste("^",input$var, sep = "")
-        bigramsFound <- bigrams[grep(term, bigrams$bigram), ]
-        bigramsFound <- bigramsFound[order(bigramsFound$Frequency, decreasing = TRUE),]
-        bigramsFound[1:3,1]
-    })
+    triPred <- trigramPrediction()
     
-    output$prediction <- renderUI({ 
-      prediction <- bigramPrediction()
-      HTML(paste(prediction[1], prediction[1], prediction[3], sep = '<br/>'))
-    })
+    if(length(triPred) > 0){
+      all <- c(as.character(triPred), all)
+    }
     
-  }
-)
+    HTML(paste(all, collapse = '<br/>'))
+  })
+  
+})
--- capstone/ui.R
+++ capstone/ui.R
@@ -1,12 +1,11 @@
 library(shiny)
-library(ggplot2)
 
 shinyUI(fluidPage(
   titlePanel("Text Predictor"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("This application will attempt to predict your next word:"),
+      helpText("This application will attempt to predict what you are going to say:"),
       
       textInput("var", label = "Enter Text")
     ),
